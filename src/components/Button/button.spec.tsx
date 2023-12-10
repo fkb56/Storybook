18885243b8af65686b89e90d6760ec76a848f70e
fkb56 <fkcb@live.fr>
2023-12-10 02:17:01 +0100

    First commit

---
+++ src/components/Button/button.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import Button from './button';
+
+describe('Spec Button', function () {
+    it('it exists', () => {
+        expect(Button).to.be.ok;
+    });
+});
